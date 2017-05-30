747ecc9c14366c5205c7c1bb34ce3f195c7e65a3
Dan Ortiz <dbortiz@csu.fullerton.edu>
2017-05-30 16:04:52 -0700

    Set up basic idea.

--- Problem4.cpp
+++ Problem4.cpp
@@ -8,7 +8,22 @@
 using namespace std;
 
 int main(){
-	
+	int a = 123456;
+	int b = 123456;
+	int arr[6], arr2[6];
+	// Original sequence
+	for(int i = 5; i >= 0; i--){
+		arr[i] = a % 10;
+		a /= 10;
+	}
 
+	// Palindrome
+	for(int i = 0; i < 6; i++){
+		arr2[i] = b % 10;
+		b /= 10;
+	}
+
+	cout << arr[5]<< endl;
+	cout << arr2[0];
 	return 0;
 }
