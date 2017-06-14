90d11cbe2b79084eae335dbda99fab607fa3684d
Dan Ortiz <dbortiz@csu.fullerton.edu>
2017-06-14 04:16:42 -0700

    Shortened divise function.

--- Problem5.cpp
+++ Problem5.cpp
@@ -7,29 +7,20 @@
 
 using namespace std;
 
-bool checkDivisibility(int);
-int smallestMult(int);
+int divise(int, int);
 
 int main(){
-	int num = 2000;
-	cout << smallestMult(num);
+	int num = 1;
+	cout << divise(num, 1);
 	return 0;
 }
 
-bool checkDivisibility(int num){
-	if(num % 1 == 0 && num % 2 == 0 && num % 3 == 0 && num % 4 == 0 && num % 5 == 0 && num % 6 == 0 && num % 7 == 0 && num % 8 == 0 &&
-	num % 9 == 0 && num % 10 == 0 ){
-		return true;
+int divise(int num, int div){
+	if(num % div == 0 && div < 10){
+		return divise(num, div + 1);
+	}else if(num % div == 0 && div == 10){
+		return num;
 	}else{
-		return false;
-	}
-
-}
-
-int smallestMult(int num){
-	if(checkDivisibility(num)){
-		 return num;
-	}else{
-		return smallestMult(num + 1);
+		return divise(num + 1, 1);
 	}
 }
